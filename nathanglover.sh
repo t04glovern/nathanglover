3e50cd7d6f9de7cbfdb79207f2e547151a3c52b2
Nathan Glover <nathan@glovers.id.au>
2017-10-22 00:54:30 +0800

    updated script and requirements to match other projects

--- nathanglover.sh
+++ nathanglover.sh
@@ -6,5 +6,12 @@ sudo systemctl stop nathanglover
 ## Git Pull
 git pull
 
+## Install pip requirements
+WORKING_DIR=/home/nathan/Production/nathanglover
+ACTIVATE_PATH=/home/nathan/.virtualenvs/flask-python2.7-dev/bin/activate
+cd ${WORKING_DIR}
+source ${ACTIVATE_PATH}
+pip install -r requirements.txt
+
 ## Restart Systemctl service
 sudo systemctl start nathanglover
