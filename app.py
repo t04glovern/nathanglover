c7dc48c8fb6ab8a580d087632df5800f17bd2869
Nathan Glover <nathan@glovers.id.au>
2018-01-02 04:02:18 +0800

    Added Ethereum charts

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, send_from_directory, jsonify
 from flask_mail import Mail, Message
+from flask_table import Table, Col
+
+import etherscan.accounts as accounts
 
 import config
 
@@ -17,6 +20,63 @@ app.config['MAIL_USE_TLS'] = config.MAIL_USE_TLS
 app.config['MAIL_USE_SSL'] = config.MAIL_USE_SSL
 mail = Mail(app)
 
+eth_wallet_address = config.ETH_WALLET_ADDRESS
+etherscan_api_key = config.ETHERSCAN_API_KEY
+etherscan_api = accounts.Account(address=eth_wallet_address, api_key=etherscan_api_key)
+
+'''
+Ethereum Table
+'''
+
+
+class EthTable(Table):
+    #tx_hash = Col('TxHash')
+    #tx_age = Col('Age')
+    tx_from = Col('From')
+    tx_to = Col('To')
+    tx_value = Col('Value')
+    #tx_fee = Col('TxFee')
+
+
+class Transaction(object):
+    def __init__(self, tx_hash, tx_age, tx_from, tx_to, tx_value, tx_fee):
+        self.tx_hash = tx_hash
+        self.tx_age = tx_age
+        self.tx_from = tx_from
+        self.tx_to = tx_to
+        self.tx_value = tx_value
+        self.tx_fee = tx_fee
+
+
+# Get the eth transactions
+eth_transactions = etherscan_api.get_transaction_page(page=1, offset=10)
+
+
+# Create and populate the eth transaction table objects
+table_transactions = []
+for transaction in eth_transactions:
+    table_transactions.append(
+        dict(#tx_hash=transaction["hash"],
+             #tx_age=transaction["timeStamp"],
+             tx_from=transaction["from"],
+             tx_to=transaction["to"],
+             tx_value=transaction["value"],
+             #tx_fee=transaction["gasUsed"]
+            )
+    )
+
+
+# Populate the table with the transaction objects
+eth_table = EthTable(table_transactions, table_id='eth_table', classes=['table-responsive', 'table'])
+
+
+'''
+Ethereum Price
+'''
+
+eth_value = etherscan_api.get_balance()
+
+
 '''
 Web Page Routes
 '''
@@ -24,7 +84,7 @@ Web Page Routes
 
 @app.route("/", methods=['GET'])
 def landing():
-    return render_template("index.html")
+    return render_template("index.html", eth_table=eth_table, eth_value=eth_value, eth_wallet=config.ETH_WALLET_ADDRESS)
 
 
 @app.route("/ajax/send_mail", methods=['POST'])
--- nathanglover.sh
+++ nathanglover.sh
@@ -8,7 +8,7 @@ git pull
 
 ## Install pip requirements
 WORKING_DIR=/home/nathan/Production/nathanglover
-ACTIVATE_PATH=/home/nathan/.virtualenvs/flask-python2.7-dev/bin/activate
+ACTIVATE_PATH=/home/nathan/.virtualenvs/flask-python3.6.3-dev/bin/activate
 cd ${WORKING_DIR}
 source ${ACTIVATE_PATH}
 pip install -r requirements.txt
